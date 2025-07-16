ab74de579bbc272aee1aa19820ddc117d1db6476
Akash Gupta <akash@truefoundry.com>
2025-07-16 20:03:35 +0530

    update

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, HTTPException
+
+from guardrail.drug_mention_guardrails_ai import drug_mention
+from guardrail.nsfw_filtering_local_eval import nsfw_filtering
+
 # from guardrail.pii_detection_guardrails_ai import pii_detection_guardrails_ai
 # from guardrail.competitor_check_guardrails_ai import competitor_check
-from guardrail.hallucination_check_guardrails_ai import hallucination_check
+# from guardrail.hallucination_check_guardrails_ai import hallucination_check
 from guardrail.pii_redaction_presidio import process_input_guardrail
-from guardrail.nsfw_filtering_local_eval import nsfw_filtering
-from guardrail.drug_mention_guardrails_ai import drug_mention
 from guardrail.web_sanitization_guardrails_ai import web_sanitization
-from guardrail.nsfw_filtering_guardrails_ai import nsfw_filtering_guardrails_ai
+
+# from guardrail.nsfw_filtering_guardrails_ai import nsfw_filtering_guardrails_ai
 
 # Create FastAPI app instance
 app = FastAPI(
@@ -18,7 +21,6 @@ app = FastAPI(
 @app.get("/")
 async def health_check():
     return {"message": "Guardrail Server is running", "version": "1.0.0"}
-
 
 
 app.add_api_route( "/pii-redaction", endpoint=process_input_guardrail, methods=["POST"])
@@ -36,8 +38,6 @@ app.add_api_route("/web-sanitization",endpoint=web_sanitization,methods=["POST"]
 # app.add_api_route("/hallucination-check",endpoint=hallucination_check,methods=["POST"])
 
 # app.add_api_route("/competitor-check",endpoint=competitor_check,methods=["POST"])
-
-
 
 
 @app.exception_handler(Exception)
