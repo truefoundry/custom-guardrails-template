013e385c6c3b95372be96e55c59ee75aae527001
Hrithik Adhikari <40512667+LordGameleo@users.noreply.github.com>
2025-07-16 19:52:41 +0530

    Refactor 3 (#12)
    * add git ignore
    
    * Disable multiple guardrails and switch to python app launch
    
    Comment out all guardrail validators and their related API routes to temporarily disable them. Update the Dockerfile to remove several guardrail hub installs and replace the server start command with a simple Python script execution.
    
    These changes simplify the setup by reducing active guardrails and changing the app startup method, likely to ease development or isolate issues.
    
    * update dockerfile

--- guardrail/hallucination_check_guardrails_ai.py
+++ guardrail/hallucination_check_guardrails_ai.py
@@ -1,17 +1,17 @@
-from typing import Optional
-from fastapi import HTTPException
-from guardrails import Guard
-from guardrails.hub import (
-    GroundedAIHallucination
-)
-from entities import OutputGuardrailRequest
+# from typing import Optional
+# from fastapi import HTTPException
+# from guardrails import Guard
+# from guardrails.hub import (
+#     GroundedAIHallucination
+# )
+# from entities import OutputGuardrailRequest
 
-guard = Guard().use(GroundedAIHallucination, on_fail="exception",quant=False)
+# guard = Guard().use(GroundedAIHallucination, on_fail="exception",quant=False)
 
-def hallucination_check(request: OutputGuardrailRequest) -> Optional[dict]:
-    try:
-        for choice in request.responseBody.get("choices", []):
-            if "content" in choice.get("message", {}):
-                guard.validate(choice["message"]["content"])
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+# def hallucination_check(request: OutputGuardrailRequest) -> Optional[dict]:
+#     try:
+#         for choice in request.responseBody.get("choices", []):
+#             if "content" in choice.get("message", {}):
+#                 guard.validate(choice["message"]["content"])
+#     except Exception as e:
+#         raise HTTPException(status_code=400, detail=str(e))
