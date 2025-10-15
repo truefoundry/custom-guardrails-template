6a717969434e9a1824eba3a579c5f6a7004388dd
Hrithik <hrithik@truefoundry.com>
2025-10-15 12:10:01 +0530

    add config based indian pii detector

--- guardrail/pii_redaction_presidio.py
+++ guardrail/pii_redaction_presidio.py
@@ -1,5 +1,10 @@
 from typing import Optional
 from presidio_analyzer import AnalyzerEngine
+from presidio_analyzer.predefined_recognizers import InAadhaarRecognizer
+from presidio_analyzer.predefined_recognizers import InPanRecognizer
+from presidio_analyzer.predefined_recognizers import InPassportRecognizer
+from presidio_analyzer.predefined_recognizers import InVehicleRegistrationRecognizer
+from presidio_analyzer.predefined_recognizers import InVoterRecognizer
 from presidio_anonymizer import AnonymizerEngine
 
 from entities import InputGuardrailRequest
@@ -7,6 +12,8 @@ from entities import InputGuardrailRequest
 # Initialize Presidio
 analyzer = AnalyzerEngine()
 anonymizer = AnonymizerEngine()
+
+adhoc_indian_recognizers = [InPanRecognizer(),InAadhaarRecognizer(),InPassportRecognizer(),InVehicleRegistrationRecognizer(),InVoterRecognizer()]
 
 def process_input_guardrail(request: InputGuardrailRequest) -> Optional[dict]:
     if not request.config.get("transform_input", False):
@@ -16,7 +23,17 @@ def process_input_guardrail(request: InputGuardrailRequest) -> Optional[dict]:
     transformed_messages = []
     for message in messages:
         if isinstance(message, dict) and message.get("content"):
-            results = analyzer.analyze(text=message["content"], entities=[], language='en')
+            if request.config.get("indian_pii_detection", False):
+                results = analyzer.analyze(
+                    text=message["content"], 
+                    language='en', 
+                    ad_hoc_recognizers=adhoc_indian_recognizers
+                )
+            else:
+                results = analyzer.analyze(
+                    text=message["content"],
+                    language='en'
+                )
             anonymized_content = anonymizer.anonymize(text=message["content"], analyzer_results=results)
             if anonymized_content.text != message["content"]:
                 transformed = True
